95199b9d33ff6330768b52f0d06cacfbcfeee3ba
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-11 21:24:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,6 +5,11 @@ require_relative './dance_module.rb'
 
 
 class Dancer
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
